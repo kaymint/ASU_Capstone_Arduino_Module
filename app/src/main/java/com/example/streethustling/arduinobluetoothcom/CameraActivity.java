a4f61d1d761e3629aa09bf6dd8c8dd6c9e170cee
kennethmensah <kenneth.mensah@ashesi.edu.gh>
2016-04-11 16:46:04 +0000

    added bluetooth module

---
+++ app/src/main/java/com/example/streethustling/arduinobluetoothcom/CameraActivity.java
@@ -0,0 +1,282 @@
+package com.example.streethustling.arduinobluetoothcom;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.provider.Settings;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import cz.msebera.android.httpclient.Header;
+
+public class CameraActivity extends AppCompatActivity {
+
+    private static final String IMAGE_DIRECTORY_NAME = "ShutterCop";
+    // Activity request codes
+    private static final int CAMERA_CAPTURE_IMAGE_REQUEST_CODE = 100;
+    public static final int MEDIA_TYPE_IMAGE = 1;
+    ProgressDialog prgDialog;
+    String encodedString;
+    RequestParams params = new RequestParams();
+    String imgPath, fileName;
+    Bitmap bitmap;
+    Button uploadBtn;
+
+
+    private Uri fileUri; // file url to store image/video
+
+    private ImageView imgPreview;
+    private Button btnCapturePicture;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+
+        imgPreview = (ImageView) findViewById(R.id.imgPreview);
+        btnCapturePicture = (Button) findViewById(R.id.btnCapturePicture);
+        prgDialog = new ProgressDialog(this);
+        // Set Cancelable as False
+        prgDialog.setCancelable(false);
+        uploadBtn = (Button) findViewById(R.id.uploadBtn);
+
+        btnCapturePicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                captureImage();
+            }
+        });
+
+
+        uploadBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                uploadImage(v);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resulCode, Intent data){
+        //request code for image capture
+        if(requestCode == CAMERA_CAPTURE_IMAGE_REQUEST_CODE){
+            if(resulCode == RESULT_OK){
+                //preview captured image
+                previewCapturedImage();
+            }
+            else if(resulCode == RESULT_CANCELED){
+                // user cancelled Image capture
+                Toast.makeText(getApplicationContext(),
+                        "User cancelled image capture", Toast.LENGTH_SHORT)
+                        .show();
+            }else{
+                // failed to capture image
+                Toast.makeText(getApplicationContext(),
+                        "Sorry! Failed to capture image", Toast.LENGTH_SHORT)
+                        .show();
+            }
+        }
+
+    }
+
+    // When Upload button is clicked
+    public void uploadImage(View v) {
+        // When Image is selected from Gallery
+        if (imgPath != null && !imgPath.isEmpty()) {
+            prgDialog.setMessage("Converting Image to Binary Data");
+            prgDialog.show();
+            // Convert image to String using Base64
+            encodeImagetoString();
+            // When Image is not selected from Gallery
+        } else {
+            Toast.makeText(
+                    getApplicationContext(),
+                    "You must select image from gallery before you try to upload",
+                    Toast.LENGTH_LONG).show();
+        }
+    }
+
+
+    public void previewCapturedImage(){
+
+        try{
+            imgPreview.setVisibility(View.VISIBLE);
+
+            BitmapFactory.Options options = new BitmapFactory.Options();
+
+            options.inSampleSize = 8;
+
+
+
+            bitmap = BitmapFactory.decodeFile(fileUri.getPath(),
+                    options);
+
+            final Bitmap bitmap1 = BitmapFactory.decodeFile(fileUri.getPath(),
+                    options);
+
+            imgPreview.setImageBitmap(bitmap1);
+
+            // Get the Image's file name
+            String fileNameSegments[] = imgPath.split("/");
+            fileName = fileNameSegments[fileNameSegments.length - 1];
+            // Put file name in Async Http Post Param which will used in Php web app
+            params.put("filename", fileName);
+
+
+
+        }catch (NullPointerException ex){
+            ex.printStackTrace();
+        }
+    }
+
+
+    /**
+     * check pressence of camera
+     * @return boolean
+     */
+    private boolean deviceHasCamera(){
+        return (getApplicationContext().getPackageManager().hasSystemFeature(
+                PackageManager.FEATURE_CAMERA));
+    }
+
+
+    private void captureImage(){
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        File imagesFolder = new File(Environment.getExternalStorageDirectory(), "MyImages");
+        imagesFolder.mkdirs(); // <----
+        // Create a media file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss",
+                Locale.getDefault()).format(new Date());
+
+        File image = new File(imagesFolder, timeStamp+".jpg");
+        fileUri  = Uri.fromFile(image);
+
+        imgPath = fileUri.getPath();
+
+        System.out.println("File URI " + imgPath);
+
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
+
+        startActivityForResult(intent, CAMERA_CAPTURE_IMAGE_REQUEST_CODE);
+
+    }
+
+
+
+    // AsyncTask - To convert Image to String
+    public void encodeImagetoString() {
+        new AsyncTask<Void, Void, String>() {
+
+            protected void onPreExecute() {
+
+            };
+
+            @Override
+            protected String doInBackground(Void... params) {
+                BitmapFactory.Options options = null;
+                options = new BitmapFactory.Options();
+                options.inSampleSize = 3;
+                bitmap = BitmapFactory.decodeFile(imgPath,
+                        options);
+                ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                // Must compress the Image to reduce image size to make upload easy
+                bitmap.compress(Bitmap.CompressFormat.PNG, 50, stream);
+                byte[] byte_arr = stream.toByteArray();
+                // Encode Image to String
+                encodedString = Base64.encodeToString(byte_arr, 0);
+                return "";
+            }
+
+            @Override
+            protected void onPostExecute(String msg) {
+                prgDialog.setMessage("Calling Upload");
+                // Put converted Image string into Async Http Post param
+                params.put("image", encodedString);
+                // Trigger Image upload
+                triggerImageUpload();
+            }
+        }.execute(null, null, null);
+    }
+
+    public void triggerImageUpload() {
+        makeHTTPCall();
+    }
+
+    // Make Http call to upload Image to Php server
+    public void makeHTTPCall() {
+        prgDialog.setMessage("Invoking Php");
+        AsyncHttpClient client = new AsyncHttpClient();
+        // Don't forget to change the IP address to your LAN address. Port no as well.
+        client.post("http://10.10.56.71:81/applied_project_upload/upload_image.php",
+                params, new AsyncHttpResponseHandler() {
+                    @Override
+                    public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                        String response = new String(responseBody);
+                        prgDialog.hide();
+                        Toast.makeText(getApplicationContext(), response,
+                                Toast.LENGTH_LONG).show();
+                    }
+
+                    @Override
+                    public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+
+                        // Hide Progress Dialog
+                        prgDialog.hide();
+                        // When Http response code is '404'
+                        if (statusCode == 404) {
+                            Toast.makeText(getApplicationContext(),
+                                    "Requested resource not found",
+                                    Toast.LENGTH_LONG).show();
+                        }
+                        // When Http response code is '500'
+                        else if (statusCode == 500) {
+                            Toast.makeText(getApplicationContext(),
+                                    "Something went wrong at server end",
+                                    Toast.LENGTH_LONG).show();
+                        }
+                        // When Http response code other than 404, 500
+                        else {
+                            Toast.makeText(
+                                    getApplicationContext(),
+                                    "Error Occured n Most Common Error: n1. Device not connected to Internetn2. Web App is not deployed in App servern3. App server is not runningn HTTP Status code : "
+                                            + statusCode, Toast.LENGTH_LONG)
+                                    .show();
+                        }
+                    }
+                });
+    }
+
+    @Override
+    protected void onDestroy() {
+        // TODO Auto-generated method stub
+        super.onDestroy();
+        // Dismiss the progress bar when application is closed
+        if (prgDialog != null) {
+            prgDialog.dismiss();
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/streethustling/arduinobluetoothcom/Simulation.java
@@ -0,0 +1,291 @@
+package com.example.streethustling.arduinobluetoothcom;
+
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.app.ProgressDialog;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.os.AsyncTask;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.util.HashMap;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.UUID;
+import android.provider.Settings.Secure;
+
+public class Simulation extends AppCompatActivity {
+
+    Button start;
+    Button disconnect;
+    Button capture;
+    TextView response;
+    BluetoothAdapter myBluetooth = null;
+    private ProgressDialog progress;
+    BluetoothSocket btSocket = null;
+    private boolean isBtConnected = false;
+    static final UUID myUUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+    private String deviceMAC = null;
+    ConnectBT connection ;
+    Timer timer;
+    private OutputStream outStream = null;
+    private InputStream in = null;
+    private Handler handler = new Handler();
+    BluetoothSocketListener bsl = null;
+    Thread messageListener = null;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_simulation);
+
+
+        start = (Button)findViewById(R.id.startBtn);
+        disconnect = (Button)findViewById(R.id.disBtn);
+        response = (TextView)findViewById(R.id.arduResponseTxt);
+        capture = (Button)findViewById(R.id.captureBtn);
+
+        //receive the address of the bluetooth device
+        Bundle extras = getIntent().getExtras();
+        if(extras != null){
+            deviceMAC = (String) extras.getString("EXTRA_ADDRESS");
+            connection = new ConnectBT();
+            connection.execute();
+        }
+
+        start.setOnClickListener(new View.OnClickListener() {
+
+
+            @Override
+            public void onClick(View v) {
+
+                startSimulation();
+            }
+        });
+
+
+        capture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startCamera();
+            }
+        });
+
+        disconnect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Disconnect();
+            }
+        });
+    }
+
+
+    private void startCamera(){
+            // Make an intent to start next activity.
+            Intent i = new Intent(Simulation.this, CameraActivity.class);
+            startActivity(i);
+    }
+
+    private void stopSimulation() throws InterruptedException {
+        if (btSocket!=null)
+        {
+            try
+            {
+                outStream.write("0".getBytes());
+            }
+            catch (IOException e)
+            {
+                msg("Error");
+            }
+        }
+    }
+
+
+    private void startSimulation()
+    {
+        if (btSocket!=null)
+        {
+            try
+            {
+                outStream.write("1".getBytes());
+
+            }
+            catch (IOException e)
+            {
+                msg("Error");
+            }
+        }
+    }
+
+
+    private void readFromArduino(){
+        if (btSocket!=null)
+        {
+            try
+            {
+                byte[] buffer = new byte[1024];
+                int bytes;
+
+                bytes = in.read(buffer);
+                String readMessage = new String(buffer, 0, bytes);
+                System.out.println("From Arduino: "+ readMessage);
+                response.append("Message: "+ readMessage + " \n");
+            }
+            catch (IOException e)
+            {
+                msg("Error");
+            }
+        }
+    }
+
+    private class MessagePoster implements Runnable {
+        private TextView textView;
+        private String message;
+
+        public MessagePoster(TextView textView, String message) {
+            this.textView = textView;
+            this.message = message;
+        }
+
+        public void run() {
+            textView.setText(message + "\n");
+            if(message.matches("ON") || message.matches(" ON") || message.matches(" ON ")){
+                startCamera();
+            }
+
+        }
+
+    }
+
+    private class BluetoothSocketListener implements Runnable {
+
+        private BluetoothSocket socket;
+        private TextView textView;
+        private Handler handler;
+
+        public BluetoothSocketListener(BluetoothSocket socket,
+                                       Handler handler, TextView textView) {
+            this.socket = socket;
+            this.textView = textView;
+            this.handler = handler;
+        }
+
+        public void run() {
+            int bufferSize = 1024;
+            byte[] buffer = new byte[bufferSize];
+            try {
+                InputStream in = socket.getInputStream();
+                int bytesRead = -1;
+                String message = "";
+                while (true) {
+                    message = "";
+                    bytesRead = in.read(buffer);
+                    if (bytesRead != -1) {
+                        while ((bytesRead==bufferSize)&&(buffer[bufferSize-1] != 0)) {
+                            message = message + new String(buffer, 0, bytesRead);
+                            bytesRead = in.read(buffer);
+                        }
+                        message = message + new String(buffer, 0, bytesRead - 1);
+                        handler.post(new MessagePoster(textView, message));
+                        socket.getInputStream();
+                    }
+                }
+            } catch (IOException e) {
+                Log.d("BLUETOOTH_COMMS", e.getMessage());
+            }
+        }
+    }
+
+
+
+    private class ConnectBT extends AsyncTask<Void, Void, Void>  // UI thread
+    {
+        private boolean ConnectSuccess = true; //if it's here, it's almost connected
+
+        @Override
+        protected void onPreExecute()
+        {
+            progress = ProgressDialog.show(Simulation.this, "Connecting...", "Please wait!!!");  //show a progress dialog
+        }
+
+        @Override
+        protected Void doInBackground(Void... devices) //while the progress dialog is shown, the connection is done in background
+        {
+            try
+            {
+                if (btSocket == null || !isBtConnected)
+                {
+                    myBluetooth = BluetoothAdapter.getDefaultAdapter();//get the mobile bluetooth device
+                    BluetoothDevice dispositivo = myBluetooth.getRemoteDevice(deviceMAC);//connects to the device's address and checks if it's available
+                    btSocket = dispositivo.createInsecureRfcommSocketToServiceRecord(myUUID);//create a RFCOMM (SPP) connection
+                    BluetoothAdapter.getDefaultAdapter().cancelDiscovery();
+                    btSocket.connect();//start connection
+                    outStream = btSocket.getOutputStream();
+                    in = btSocket.getInputStream();
+                }
+            }
+            catch (IOException e)
+            {
+                ConnectSuccess = false;//if the try failed, you can check the exception here
+            }
+            return null;
+        }
+        @Override
+        protected void onPostExecute(Void result) //after the doInBackground, it checks if everything went fine
+        {
+            super.onPostExecute(result);
+
+            if (!ConnectSuccess)
+            {
+                Toast.makeText(getApplicationContext(), "Connection Failed. Is it a SPP Bluetooth? Try again.", Toast.LENGTH_LONG).show();
+                response.append("Connection Failed \n");
+                finish();
+            }
+            else
+            {
+                Toast.makeText(getApplicationContext(), "Connected.", Toast.LENGTH_LONG).show();
+                response.append("Connected \n");
+                isBtConnected = true;
+                bsl = new BluetoothSocketListener(btSocket, handler, response);
+                messageListener = new Thread(bsl);
+                messageListener.start();
+            }
+            progress.dismiss();
+        }
+    }
+
+
+    private void msg(String s)
+    {
+        Toast.makeText(getApplicationContext(),s,Toast.LENGTH_LONG).show();
+    }
+
+
+    private void Disconnect()
+    {
+        if (btSocket!=null) //If the btSocket is busy
+        {
+            try
+            {
+                btSocket.close(); //close connection
+            }
+            catch (IOException e)
+            { msg("Error");}
+        }
+        finish(); //return to the first layout
+    }
+}
